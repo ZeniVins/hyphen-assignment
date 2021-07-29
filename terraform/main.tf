5cf5eac65f4c2a3a977589ef9b7b82fffe55197d
Lina <lina.lau@decathlon.com>
2021-07-29 12:17:56 +0800

    start minikube

---
+++ terraform/main.tf
@@ -0,0 +1,36 @@
+provider "kubernetes" {
+  config_path    = "~/.kube/config"
+}
+
+resource "kubernetes_pod" "mypod" {
+  metadata {
+    name = "mypod"
+    labels = {
+      app = "MyApp"
+    }
+  }
+  spec {
+    container {
+      image = "simpledocker:v1"
+      name  = "webserver"
+    }
+  }
+}
+
+resource "kubernetes_service" "myservice" {
+  metadata {
+    name = "myservice"
+  }
+  spec {
+    selector = {
+      app = kubernetes_pod.mypod.metadata.0.labels.app
+    }
+    session_affinity = "ClientIP"
+    port {
+      port        = 80
+      target_port = 80
+    }
+
+    type = "NodePort"
+  }
+}
